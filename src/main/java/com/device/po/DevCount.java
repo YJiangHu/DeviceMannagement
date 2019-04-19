4743533e2ccaace9dda7c5eb70aabf25ceb5dfba
hyf <1072472766@qq.com>
2019-04-19 22:10:42 +0800

    修改时间类型为String

---
+++ src/main/java/com/device/po/DevCount.java
@@ -0,0 +1,69 @@
+package com.device.po;
+
+public class DevCount {
+    private String devBasicNo;
+    private String devBasicName;
+    private String devBasicBelong;
+    private String devBasicQRcode;
+    private String devBasicLocation;
+    private Integer devBasicAdminID;
+    private Integer devBasicClassify;
+
+    public String getDevBasicNo() {
+        return devBasicNo;
+    }
+
+    public void setDevBasicNo(String devBasicNo) {
+        this.devBasicNo = devBasicNo;
+    }
+
+    public String getDevBasicName() {
+        return devBasicName;
+    }
+
+    public void setDevBasicName(String devBasicName) {
+        this.devBasicName = devBasicName;
+    }
+
+    public String getDevBasicBelong() {
+        return devBasicBelong;
+    }
+
+    public void setDevBasicBelong(String devBasicBelong) {
+        this.devBasicBelong = devBasicBelong;
+    }
+
+    public String getDevBasicQRcode() {
+        return devBasicQRcode;
+    }
+
+    public void setDevBasicQRcode(String devBasicQRcode) {
+        this.devBasicQRcode = devBasicQRcode;
+    }
+
+    public String getDevBasicLocation() {
+        return devBasicLocation;
+    }
+
+    public void setDevBasicLocation(String devBasicLocation) {
+        this.devBasicLocation = devBasicLocation;
+    }
+
+    public Integer getDevBasicAdminID() {
+        return devBasicAdminID;
+    }
+
+    public void setDevBasicAdminID(Integer devBasicAdminID) {
+        this.devBasicAdminID = devBasicAdminID;
+    }
+
+    public Integer getDevBasicClassify() {
+        return devBasicClassify;
+    }
+
+    public void setDevBasicClassify(Integer devBasicClassify) {
+        this.devBasicClassify = devBasicClassify;
+    }
+
+
+}
