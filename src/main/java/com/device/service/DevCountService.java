2b2d75146413c49c98f0f93749c9aa9e7d4398d8
hyf <1072472766@qq.com>
2019-04-20 21:03:06 +0800

    更新前端部分文件

---
+++ src/main/java/com/device/service/DevCountService.java
@@ -0,0 +1,11 @@
+package com.device.service;
+
+import com.device.po.DevCount;
+import com.device.po.DevLog;
+
+import java.util.List;
+
+public interface DevCountService {
+
+    List<DevCount> listDevCounts();
+}
